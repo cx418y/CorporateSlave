78fa8d87502127667adccb490ae85f98712eb90d
lilonghai101 <514512586@qq.com>
2024-05-01 16:42:54 +0800

    longhai first commit

---
+++ Assets/Scripts/Data/Day.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Day 
+{
+    public string currentDay;
+    public int numberDay;
+    //public string CurrentDay { get => currentDay; set => currentDay = value; }
+   // public int NumberDay { get => numberDay; set => numberDay = value; }
+}
---
+++ Assets/Scripts/Data/dataSave.cs
@@ -0,0 +1,116 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+
+public class DayList {
+    public List<Day> list = new List<Day>();
+}
+
+
+public class dataSave : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    int currentDay=1;
+    public Day day;
+    public int number=1;
+    public   DayList list = new DayList();
+   // public List<Day>dayList=new List<Day>();
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //开始新游戏
+  public  void StartData() { 
+    day = new Day();
+        day.numberDay = number;
+        day.currentDay = "第"+day.numberDay +"天";
+      //  Debug.Log(day.numberDay+day.currentDay);
+        list.list.Add(day);
+       // Debug.Log(list.list[0].currentDay);
+          string json = JsonUtility.ToJson(list);
+       
+        Debug.Log(json);
+        if (!File.Exists(Application.streamingAssetsPath + "/dataList.json"))
+        {
+           string filepath = Application.streamingAssetsPath + "/dataList.json";
+        using (StreamWriter writer = new StreamWriter(filepath))
+        {
+            writer.WriteLine(json);
+            writer.Close();
+            writer.Dispose();
+        } 
+        }
+        
+    }
+    //添加数据
+   public void SaveData()
+    {
+        string filepath = Application.streamingAssetsPath + "/dataList.json";
+        if (File.Exists(filepath))
+        {
+            day = new Day();
+            int current = currentDay;
+            current++;
+            day.numberDay = current;
+            day.currentDay = "第" + day.numberDay + "天";
+            list.list.Add(day);
+            string json = JsonUtility.ToJson(list);
+            //Debug.Log(json);
+          
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                writer.WriteLine(json);
+                writer.Close();
+                writer.Dispose();
+            }
+        }
+    }
+    //读取数据
+  public  void ReadData()
+    {
+        string nowDay=null;
+        string json;
+        string filepath = Application.streamingAssetsPath + "/dataList.json";
+        if (File.Exists(filepath))
+        {
+            using (StreamReader writer = new StreamReader(filepath))
+            {
+                json = writer.ReadToEnd();
+                writer.Close();
+                writer.Dispose();
+            }
+            list = JsonUtility.FromJson<DayList>(json);
+            foreach (Day day in list.list)
+            {
+              //  Debug.Log(day.currentDay);
+                currentDay = day.numberDay;
+                nowDay = day.currentDay;
+               // Debug.Log(nowDay);
+            }
+            if (nowDay != null)
+            {
+                Text.text = nowDay;
+            }
+        }
+        else
+        {
+            Text.text ="第1天";
+        }
+    } 
+    public void RemoveData() 
+    {
+        if (File.Exists(Application.streamingAssetsPath + "/dataList.json"))
+        {
+            File.Delete(Application.streamingAssetsPath + "/dataList.json");
+        }
+    }
+ 
+}

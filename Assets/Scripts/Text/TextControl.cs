dd2c86c5efc42d17bd20a939408b999db797c1c6
cx418y <19302010084@fudan.edu.cn>
2024-05-14 23:07:33 +0800

    邮件系统

---
+++ Assets/Scripts/Text/TextControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextControl : MonoBehaviour
+{
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
+    public GameObject Text4;
+    public GameObject Text5;
+    public GameObject Text6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
